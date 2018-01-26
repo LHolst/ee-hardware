257187489a8c4e2d872ce9f941692f2ae61ac9cb
Leonard Holst <holst@geoplex.de>
2018-01-26 18:17:03 +0100

    add channels section

--- ee_hardware/cli.py
+++ ee_hardware/cli.py
@@ -2,21 +2,30 @@ import click
 
 class sACNDeviceHardware():
     def __init__(self, num_leds):
-        self.device = 'sACNDevice'
-        self.num_leds = 1
-        self.color_per_led = 3
-        self.channels = self.color_per_led*self.num_leds
+        self._channel_prefix_single = 'p'
+        self._device = 'sACNDevice'
+        self._num_leds = num_leds
+        self._color_per_led = 3
+        self._channels = self._color_per_led * self._num_leds
 
     @property
     def hardware(self):
-        s_name = 'hardware'
-        out = f'[{s_name}]\n'
-        out +=f'device = {self.device}\n'
-        out +=f'channels = {self.channels}\n'
+        out = f'[hardware]\n'
+        out +=f'device = {self._device}\n'
+        out +=f'channels = {self._channels}\n'
+        return out
+
+    @property
+    def channels(self):
+        out = f'[channels]\n'
+        for ch in range(self._num_leds):
+            r,g,b = [(ch)*3+i+1 for i in range(3)]
+            out += f'{self._channel_prefix_single}{ch} = {r}, {g}, {b}\n'
         return out
 
 @click.command()
-@click.option('--num_leds','-n', default=1)
+@click.option('--num_leds','-n', default=8)
 def ee_hardware(num_leds):
     d = sACNDeviceHardware(num_leds)
     click.echo(d.hardware)
+    click.echo(d.channels)
